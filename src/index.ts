3c9f63d22b6fd79136e50149d618782ec5b5269a
ConstasJ <2020212726@qq.com>
2021-10-31 00:37:07 +0800

    尝试ing

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ export const name = 'yuque'
 
 interface BasicOptions {
     WebHook?: {
+        assignee: string
+        sendto: string[]
     }
 }
 
 export function apply(ctx: Context, config: BasicOptions = {}) {
-
+    const sendto = ['onebot:966480341']
+    const mainbot = ctx.bots[`onebot:3585366684`];
+    ctx.router.post('/yuque', (ctxn) => {
+        const eventBody = ctxn.request.body
+        for (let channel of sendto) mainbot.sendMessage(channel.slice(7), eventBody)
+    })
 }
