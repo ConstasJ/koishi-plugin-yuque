0ef6f8de58f21898f8aed792c0cb8e53f3317204
ConstasJ <2020212726@qq.com>
2021-10-31 00:29:52 +0800

    迷茫中。。。

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,12 @@
-import { Context } from 'koishi';
-import e from "express";
-import bodyParser from "body-parser";
-
-const app = e()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+import { Context, Session } from 'koishi';
 
 export const name = 'yuque'
 
 interface BasicOptions {
     WebHook?: {
-        port: number
     }
 }
 
-export function apply(ctx: Context, config: BasicOptions = { WebHook: { port: 9000 } }) {
-    let webHook;
-    //@ts-expect-error
-    const mainbot = ctx.getBot('onebot', '3585366684')
-    ctx.on('connect', () => {
-        app.listen(config.WebHook.port, () => { })
-        app.post('/yuque', (req, res, next) => {
-            webHook = {
-                id: req.body.id,
-                slug: req.body.slug,
-                title: req.body.title,
-                book_id: req.body.book_id
-            }
-            mainbot.sendMessage('966480341', JSON.stringify(webHook))
-        })
-    })
+export function apply(ctx: Context, config: BasicOptions = {}) {
+
 }
