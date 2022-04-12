9b4d4a569758ad483d071417667c1a99762c1928
ConstasJ <2020212726@qq.com>
2022-04-12 16:10:19 +0800

    feat: 两项重大更改
    完成updateDB()函数和.is指令

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,11 +1,16 @@
 import {Context,Session,Logger} from "koishi";
+import {updateDB} from "./update";
 
 async function cmd(ctx:Context,log:Logger){
     const cmd=ctx.command('yuquepush','语雀推送相关功能',{authority:3});
     cmd.alias('yqps');
     cmd.subcommand('.is','查看该频道是否为语雀推送频道',{authority:3})
-        .action(({session})=>{
-
+        .action(async ({session})=>{
+            const res=await updateDB(ctx);
+            for(const r of res){
+                if(r.platform===session.platform && r.id===session.id && r.yuque === 1) return '本频道是语雀推送频道';
+            }
+            return '本频道不是语雀推送频道';
         })
 }
 
--- src/update.ts
+++ src/update.ts
@@ -1,5 +1,7 @@
 import {Context} from "koishi";
 
 async function updateDB(ctx:Context){
-
+    return await ctx.database.getAssignedChannels(['platform','id','yuque']);
 }
+
+export {updateDB}
