cd3b4696d5fb94f1527e4c60543b6666cd0bbdd3
ConstasJ <2020212726@qq.com>
2022-04-17 22:10:30 +0800

    feat: 完成指令操作

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,5 +1,4 @@
 import {Context,Session,Logger} from "koishi";
-import {updateDB} from "./update";
 import {Config} from "./types";
 
 async function cmd(ctx:Context,log:Logger,conf:Config){
@@ -10,6 +9,30 @@ async function cmd(ctx:Context,log:Logger,conf:Config){
             const rn=await ctx.database.getChannel(session.platform,session.channelId);
             if(rn.yuque==1 || conf.list.includes(`${session.platform}:${session.channelId}`)) return '本频道是语雀推送频道';
             return '本频道不是语雀推送频道';
+        })
+    cmd.subcommand('.add','添加语雀推送频道',{authority:3})
+        .action(async ({session})=>{
+            try{
+                await ctx.database.upsert('channel',[
+                    { platform:session.platform,id:session.channelId,yuque:1}
+                ]);
+                return '设置成功！';
+            }catch (e) {
+                log.error(e);
+                return '出现错误！详情请查看日志';
+            }
+        });
+    cmd.subcommand('.del','添加语雀推送频道',{authority:3})
+        .action(async ({session})=>{
+            try{
+                await ctx.database.upsert('channel',[
+                    { platform:session.platform,id:session.channelId,yuque:0}
+                ]);
+                return '设置成功！';
+            }catch (e) {
+                log.error(e);
+                return '出现错误！详情请查看日志';
+            }
         })
 }
 
--- src/update.ts
+++
@@ -1,7 +0,0 @@
-import {Context} from "koishi";
-
-async function updateDB(ctx:Context){
-    return await ctx.database.getAssignedChannels(['platform','id','yuque']);
-}
-
-export {updateDB}
