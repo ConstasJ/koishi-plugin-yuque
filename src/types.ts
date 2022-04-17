92861773e516e1a57e3b02d8bb9dbdd133595911
ConstasJ <2020212726@qq.com>
2022-04-17 23:34:05 +0800

    refactor: 更新Type Enum的推送消息类型

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,11 @@ enum Type {
     UPDATE_DOC = '文档更新',
     CREATE_DOC = '文档发布',
     DELETE_DOC = '文档删除',
+    CREATE_COMMENT = '新增评论',
+    UPDATE_COMMENT = '更新评论',
+    NEW_REVIEW = '新建评审',
+    COMPLETE_REVIEW = '完成评审',
+    CANCEL_REVIEW = '取消评审',
 }
 
-export {Config,Type}
+export {Config, Type}
