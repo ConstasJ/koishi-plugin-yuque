175531e24f531f75d2bb5c09da74406591d4833d
ConstasJ <2020212726@qq.com>
2022-07-05 23:07:26 +0800

    test: complete some

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -12,3 +12,9 @@ app.plugin(apply);
 before(async () => {
     await app.start();
 })
+
+describe('Yuque push plugin for Koishi.js', () => {
+    it('Should correctly get push', async () => {
+        await app.http.post('http://localhost:35565/api/yuque', {})
+    })
+})
